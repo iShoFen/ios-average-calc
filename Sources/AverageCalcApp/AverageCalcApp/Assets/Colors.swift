5c4d9912a9ad739f85e9821c525d118a9ec4e2c3
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-26 02:58:42 +0200

    🍱 add light grey color

---
+++ Sources/AverageCalcApp/AverageCalcApp/Assets/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  AverageCalcApp
+//
+//  Created by etudiant on 25/05/2023.
+//
+
+import Foundation
+import SwiftUI
+
+public struct CalcColors {
+    static let lightGray = Color("lightGrey")
+}
