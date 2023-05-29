706c515bdd8b5b7ab346e44f9e5523bd96c96cdf
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-29 02:24:13 +0200

    💄 update the ue detail page
    - Add the EditView sheet
    - Add some update check for the courses or the ue name with alerts to inform the user

--- Sources/AverageCalcApp/AverageCalcApp/UI/Pages/UEDetailPage.swift
+++ Sources/AverageCalcApp/AverageCalcApp/UI/Pages/UEDetailPage.swift
@@ -11,24 +11,29 @@ import AverageCalcViewModel
 
 struct UEDetailPage: View {
     @ObservedObject var ueVM: UEVM
-    @ObservedObject var uesVM: UEsVM
+    @ObservedObject var ucaVM : UCAVM
+    @State private var coursesError = false
+    @State private var ueError = false
 
     var body: some View {
-        ScrollView() {
+        ScrollView {
             VStack(alignment: .leading, spacing: 32) {
-                UEItemView(ueVM: ueVM)
+                UEItemView(ueData: ueVM.original.data)
 
                 VStack(alignment: .leading, spacing: 8) {
                     Label("coefficient : \(ueVM.original.coefficient.format(f: ".1"))", systemImage: "xmark.circle.fill")
                     Label("Détails des notes", systemImage: "note.text")
                 }
-                
+
                 CoursesView(ueVM: ueVM)
             }
         }
         .padding(.horizontal, 8)
         .padding(.vertical)
         .navigationTitle(ueVM.original.name)
+        .onDisappear {
+            _ = ucaVM.updateUE(with: ueVM)
+        }
         .toolbar {
             ToolbarItem(placement: .primaryAction) {
                 Button {
@@ -38,21 +43,35 @@ struct UEDetailPage: View {
                 }
             }
         }
-        .sheet(isPresented: $ueVM.isEditing) {
+        .sheet(isPresented: $ueVM.isEditing, onDismiss: {
+            _ = ueVM.onEdited(isCancelled: true)
+        }) {
             NavigationStack {
-                Text("Page d'édition d'un UE")
+                UEEditView(ueData: $ueVM.model)
                     .toolbar {
                         ToolbarItem(placement: .confirmationAction) {
                             Button("Done") {
-                               _ = ueVM.onEdited()
+                                ueError = !ucaVM.checkUENameAvailability(name: ueVM.model.name)
+
+                                if !ueError {
+                                    coursesError = !ueVM.onEdited()
+                                }
+                            }
+                            .alert("Au moins un de vos cours possède le 2 fois le même nom. Veuillez les modifier afin de pouvoir sauvegarder les changements", isPresented: $coursesError) {
+                                Button("OK", role: .cancel) {}
+                            }
+                            .alert("Le nom de l'UE est déjà utilisé par une autre? Veuillez le modifier afin de pouvoir sauvegarder les changements", isPresented: $ueError) {
+                                Button("OK", role: .cancel) {}
                             }
                         }
+                        
                         ToolbarItem(placement: .cancellationAction) {
                             Button("Cancel") {
-                                _ = ueVM.onEdited(isCancelled: true)
+                                    _ = ueVM.onEdited(isCancelled: true)
+                                
                             }
                         }
-                     }
+                    }
             }
         }
     }
@@ -60,10 +79,10 @@ struct UEDetailPage: View {
 
 struct UEDetailPage_Previews: PreviewProvider {
     static var previews: some View {
-        let uesVM = UEsVM(withUEs: loadAllUEs())
-        let ueVM = UEVM(fromUE: uesVM.ues[0])
+        let ucaVM = UCAVM(withBlock: loadAllBlocks())
+        let ueVM = UEVM(fromUE: ucaVM.blocks[0].ues[0])
         NavigationStack {
-            UEDetailPage(ueVM: ueVM, uesVM: uesVM)
+            UEDetailPage(ueVM: ueVM, ucaVM: ucaVM)
         }
     }
 }
